75e8b7d4c9856dc0546ada1fa3da9cb05212cff8
GinOfSand <96676512+GinOfSand@users.noreply.github.com>
2022-07-06 18:06:38 +0700

    Добавьте файлы проекта.

---
+++ DZ_Claas_3/MassFiltr.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_Claas_3
+{
+    internal class MassFiltr
+    {   
+       public static int[] MF_mass(int [] mOriginal, int [] mFilter)
+        {
+            int[]mass = new int[mOriginal.Length];
+            int  flag = 0;
+            int  size = 0;
+            for(int i = 0; i <mOriginal.Length; i++)
+            {
+                for(int j = 0; j < mFilter.Length; j++)
+                {
+                    if(mOriginal[i] == mFilter[j])
+                    {   
+                        flag++;
+                    }
+                }
+                if (flag == 0)
+                {
+                    mass[i] = mOriginal[i];
+                    size++;
+                }
+                flag = 0;
+            }
+            int j1 = 0;
+            int[] mass2 = new int[size];
+            for(int i = 0; i < mass.Length; i++)
+            {   
+                
+                    if (mass[i] != 0)
+                    {
+                        mass2[j1] = mass[i];
+                        j1++;
+                    }
+                
+            }
+            return mass2;
+        }
+    }
+}
---
+++ DZ_Claas_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_Claas_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Original");
+            int[] m1 = { 1, 2, 3, 2, 5, 2, -4 };
+            foreach (int i in m1)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Filter");
+            int[] m2 = { 1, 2 };
+            foreach (int i in m2) 
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Result");
+            foreach (int i in MassFiltr.MF_mass(m1, m2)) { 
+                Console.Write(i+" "); 
+            }
+            Console.WriteLine();
+        }
+    }
+}
